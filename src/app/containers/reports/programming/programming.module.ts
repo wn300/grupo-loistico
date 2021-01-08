ad105004edd0f3526c643b62fa8e62e4646cb21f
wmanceranuma93 <wilmer.mancera@numa.com.co>
2021-01-07 21:10:18 -0500

    report programming with match reports users app

--- src/app/containers/reports/programming/programming.module.ts
+++ src/app/containers/reports/programming/programming.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 import { ProgrammingRoutingModule } from './programming-routing.module';
 import { ProgrammingComponent } from './programming.component';
@@ -10,8 +19,17 @@ import { ProgrammingComponent } from './programming.component';
   declarations: [ProgrammingComponent],
   imports: [
     CommonModule,
+    FormsModule,
     MatCardModule,
-    ProgrammingRoutingModule
+    ProgrammingRoutingModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSelectModule
   ]
 })
 export class ProgrammingModule { }
