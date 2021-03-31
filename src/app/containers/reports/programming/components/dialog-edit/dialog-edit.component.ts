9e3568e2e735d170d4329c7f8206ed84198c5aef
wmanceranuma93 <wilmer.mancera@numa.com.co>
2021-03-30 22:33:43 -0500

    motivo de modificacion y usuario que modifica agreggado al reporte de programacion

--- src/app/containers/reports/programming/components/dialog-edit/dialog-edit.component.ts
+++ src/app/containers/reports/programming/components/dialog-edit/dialog-edit.component.ts
@@ -1,43 +1,52 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import * as _ from 'lodash';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogFormComponent } from 'src/app/containers/administration/client/components/dialog-form/dialog-form.component';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ProgrmmingService } from '../../services/progrmming.service';
 
 @Component({
   selector: 'app-dialog-edit',
   templateUrl: './dialog-edit.component.html',
   styleUrls: ['./dialog-edit.component.scss']
 })
-export class DialogEditComponent implements OnInit {
+export class DialogEditComponent {
   public objectEdit: any;
   public startDate: Date;
   public endDate: Date;
 
   constructor(public dialogRef: MatDialogRef<DialogFormComponent>,
-    @Inject(MAT_DIALOG_DATA) public form: any) {
+    @Inject(MAT_DIALOG_DATA) public form: any, private firebaseAuth: AngularFireAuth, public progrmmingService: ProgrmmingService) {
     const onlyDateNow = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
-    console.log(this.form.data.dateInit, this.form.data.dateEnd);
+    this.objectEdit = null;
 
-    this.objectEdit = {
-      dialogForm: {
-        startDate: this.form.data.dateInit === 'No Registra'
-          ? moment(onlyDateNow).toDate()
-          : _.clone(new Date(this.form.data.dateInit)),
-        endDate: this.form.data.dateEnd === 'No Registra'
-          ? moment(onlyDateNow).toDate()
-          : _.clone(this.form.data.dateEnd),
-        observation: _.clone(this.form.data.observation),
-        reason: '',
-      },
-      ...this.form.data
-    };
+    this.firebaseAuth.authState.subscribe(authState => {
+      this.progrmmingService.getOnlyPeopleByUID(authState.uid)
+        .subscribe(people => {
+          console.log(people);
+          
+          this.objectEdit = {
+            dialogForm: {
+              startDate: this.form.data.dateInit === 'No Registra'
+                ? moment(onlyDateNow).toDate()
+                : _.clone(new Date(this.form.data.dateInit)),
+              endDate: this.form.data.dateEnd === 'No Registra'
+                ? moment(onlyDateNow).toDate()
+                : _.clone(this.form.data.dateEnd),
+              observation: _.clone(this.form.data.observation),
+              reason: '',
+              userUpdate: `${people[0].firstName} ${people[0].firstLastName}`,
+              dateUppdate: onlyDateNow
+            },
+            ...this.form.data
+          };
 
-    console.log(this.objectEdit);
+          console.log(this.objectEdit);
+        });
 
-  }
+    });
 
-  ngOnInit(): void {
   }
 
   onNoClick(): void {
